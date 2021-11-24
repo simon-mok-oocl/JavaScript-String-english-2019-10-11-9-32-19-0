2d10a3ac5eb67afb4793be252a6983532fa470df
simon mok <simon.mok@oocl.com>
2021-11-24 22:38:58 +0800

    ans

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+name = name.toUpperCase();
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+sentence =  sentence.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.replace('￥','')
